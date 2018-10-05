3e97debb27d9285ccb2a45c5ebb49c8ce22dbf9a
Pankaj Pandav <pankaj.pandav@gruden.com>
2018-10-05 14:42:11 +1000

    Finished Product

--- handler.py
+++ handler.py
@@ -1,58 +1,32 @@
+# Delete snapshots older than retention period
+
 import boto3
 import os
-from datetime import datetime
-import time
-import logging
-import re
-from snapshots_tool_utils import *
-
-
-#Initialize everything
-#DEST_REGION = os.getenv('DEST_REGION', os.getenv('region')).strip()
-LOGLEVEL = os.getenv('LOG_LEVEL', 'ERROR').strip()
-PATTERN = os.getenv('PATTERN', 'ALL_INSTANCES')
-RETENTION_DAYS =  int(os.getenv('RETENTION_DAYS','60'))
-TIMESTAMP_FORMAT = '%Y-%m-%d-%H-%M'
-
-#if os.getenv('REGION_OVERRIDE', 'NO') != 'NO' :
-#    REGION = os.getenv('REGION_OVERRIDE').strip()
-#else:
-#    REGION = os.getenv('AWS_DEFAULT_REGION')
-
-logger = logging.getLogger()
-logger.setLevel(LOGLEVEL.upper())
-
+from botocore.exceptions import ClientError
+from datetime import datetime,timedelta
 
 def snapShotDeleteRDS(event, context):
-    pending_delete = 0
-    #search for all snapshots
-    client = boto3.client('rds')
-    response = paginate_api_call(client, 'describe_db_snapshots', 'DBSnapshots')
-    filtered_list = get_own_snapshots_source(PATTERN, response)
 
-    for snapshot in filtered_list.keys():
-        creation_date = get_timestamp(snapshot, filtered_list)
-        if creation_date:
-            difference = datetime.now() - creation_date
-            days_difference = difference.total_Seconds() / 3600 / 24
-            logger.debug('%s created %s days ago' % (snapshot, days_difference))
+    # Get current timestamp in UTC
+    now = datetime.now()
+	# Define retention period in days
+    retention_days = 60
 
-            # if we are past RETENTION_DAYS
-            if days_difference > RETENTION_DAYS:
-                #delete it
-                logger.info('Deleting %s' % snapShotDeleteRDS)
+    # Create RDS client
+    rds = boto3.client('rds')
 
-                try:
-                    client.delete_db_snapshot(DBSnapshotIdentifier=snapshot)
+    result = rds.describe_db_snapshots()
 
-                except Exception:
-                    pending_delete += 1
-                    logger.info('Could not delete %s ' % snapshot)
+    for snapshot in result['DBSnapshots']:
+        print "Checking snapshot %s which was created on %s" % (snapshot['DBSnapshotIdentifier'],snapshot['SnapshotCreateTime'])
 
+        # Remove timezone info from snapshot in order for comparison to work below
+        snapshot_time = snapshot['SnapshotCreateTime'].replace(tzinfo=None)
+        DBSnapID = snapshot['DBSnapshotIdentifier']
+        # Subtract snapshot time from now returns a timedelta
+        # Check if the timedelta is greater than retention days
+        if (now - snapshot_time) > timedelta(retention_days):
+            print "Snapshot is older than configured retention of %d days..deleting it!" % (retention_days)
+            rds.delete_db_snapshot(DBSnapshotIdentifier = DBSnapID)
         else:
-            logger.info('Not deleting %s . Creaed only %s ' % (snapshot, days_difference))
-
-    if pending_delete > 0:
-        message = 'Snapshots pending delete: %s ' % pending_delete
-        logger.error(message)
-        #raise SnapshotToolException(message)
+            print "Snapshot is newer than configured retention of %d days so we keep it" % (retention_days)
